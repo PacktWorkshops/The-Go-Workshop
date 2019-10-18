a42f0e2561d86a7d33f7d345eb60372f5653d62a
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2019-10-18 14:11:31 +1100

    Adding example and exercises for Chapter20

---
+++ Chapter20/Examples/example3/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	runDeepEqual(nil, nil)
+	runDeepEqual(make([]int, 10), make([]int, 10))
+	runDeepEqual([3]int{1, 2, 3}, [3]int{1, 2, 3})
+	runDeepEqual(map[int]string{1: "one", 2: "two"}, map[int]string{2: "two", 1: "one"})
+}
+
+func runDeepEqual(a, b interface{}) {
+	fmt.Printf("%v DeepEqual %v : %v\n", a, b, reflect.DeepEqual(a, b))
+}
