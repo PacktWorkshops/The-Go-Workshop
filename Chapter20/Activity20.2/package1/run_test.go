29392be9eb528de3decbe6135c40ba3bdb1b31f8
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2019-10-18 16:06:21 +1100

    Adding Activity20.2

---
+++ Chapter20/Activity20.2/package1/run_test.go
@@ -0,0 +1,7 @@
+package package1
+
+import "testing"
+
+func TestPackage1(t *testing.T) {
+	t.Log("running TestPackage1")
+}
---
+++ Chapter20/Activity20.2/package2/run_test.go
@@ -0,0 +1,7 @@
+package package2
+
+import "testing"
+
+func TestPackage2(t *testing.T) {
+	t.Log("running TestPackage2")
+}
