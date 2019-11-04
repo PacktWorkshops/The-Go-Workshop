772e994efb73009e3986c2fc3f898b49a3289059
PournamiJ <52200121+PournamiJ@users.noreply.github.com>
2019-11-04 12:24:16 +0530

    Update main.go

--- Chapter05/Activity5.01/main.go
+++ Chapter05/Activity5.01/main.go
@@ -1,21 +1,16 @@
 package main
-
 import "fmt"
-
 type Developer struct {
 	Individual Employee
 	HourlyRate int
 	WorkWeek   [7]int
 }
-
 type Employee struct {
 	Id        int
 	FirstName string
 	LastName  string
 }
-
 type Weekday int
-
 const (
 	Sunday Weekday = iota //starts at zero
 	Monday
@@ -25,7 +20,6 @@ const (
 	Friday
 	Saturday
 )
-
 func main() {
 	d := Developer{Individual: Employee{Id: 1, FirstName: "Tony", LastName: "Stark"}, HourlyRate: 10}
 	d.LogHours(Monday, 8)
@@ -34,11 +28,9 @@ func main() {
 	fmt.Println("Hours worked on Tuesday:  ", d.WorkWeek[Tuesday])
 	fmt.Printf("Hours worked this week:  %d", d.HoursWorked())
 }
-
 func (d *Developer) LogHours(day Weekday, hours int) {
 	d.WorkWeek[day] = hours
 }
-
 func (d *Developer) HoursWorked() int {
 	total := 0
 	for _, v := range d.WorkWeek {
