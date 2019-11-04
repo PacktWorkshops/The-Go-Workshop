0458b5a9a4e053188c923f6f41baef1354bcce79
PournamiJ <52200121+PournamiJ@users.noreply.github.com>
2019-11-04 15:08:21 +0530

    Update main.go

--- Chapter05/Exercise5.03/main.go
+++ Chapter05/Exercise5.03/main.go
@@ -1,16 +1,13 @@
 package main
-
 import (
 	"fmt"
 )
-
 func main() {
 	for i := 1; i <= 15; i++ {
 		n, s := fizzBuzz(i)
 		fmt.Printf("Results:  %d %s\n", n, s)
 	}
 }
-
 func fizzBuzz(i int) (int, string) {
 	switch {
 	case i%15 == 0:
