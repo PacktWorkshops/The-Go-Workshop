a6043407eb899c92d493339954adfe0bb0f66bf1
PournamiJ <52200121+PournamiJ@users.noreply.github.com>
2019-11-04 17:28:26 +0530

    Update DBInit.go

--- Chapter13/Examples/DBInit.go
+++ Chapter13/Examples/DBInit.go
@@ -1,11 +1,9 @@
 package main
-
 import "fmt"
 import "database/sql"
 import _ "github.com/lib/pq" 
 func main(){
 	db, err := sql.Open("postgres", "user=postgres password=Start!123 host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
-	
 	if err != nil {
 	  panic(err)
 	}else{
@@ -25,7 +23,6 @@ DBCreate := `
 	ALTER TABLE public.test
 		OWNER to postgres;
 	`
-	
 	_,err = db.Exec(DBCreate)
 	if err != nil {
 		panic(err)
@@ -33,5 +30,4 @@ DBCreate := `
 		fmt.Println("The table was successfully created!")
 	}
 	db.Close()
-}
-	
+}	
