c33d8a2703ead5eb8af38e3b49c61ba0574ca029
PournamiJ <52200121+PournamiJ@users.noreply.github.com>
2019-11-11 16:22:39 +0530

    Update main.go

--- Chapter13/Activity13.01/main.go
+++ Chapter13/Activity13.01/main.go
@@ -1,41 +1,29 @@
 package main
-
 import "fmt"
 import "database/sql"
 import _ "github.com/lib/pq"
-
 type Users struct {
     id int
     name string
     email string
 }
-
-
 func main(){
-
 users := []Users{
 	{1,"Szabo Daniel","daniel@packt.com"},
 	{2,"Szabo Florian","florian@packt.com"},
 }
-
-
-
 db, err := sql.Open("postgres", "user=postgres password=Start!123 host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
-
 if err != nil {
   panic(err)
 }else{
 	fmt.Println("The connection to the DB was successfully initialized!")
 }
-
 connectivity := db.Ping()
 if connectivity != nil{
 	panic(connectivity)
 }else{
 	fmt.Println("Good to go!")
 }
-
-
 TableCreate := `
 CREATE TABLE users
 (
@@ -52,7 +40,6 @@ TABLESPACE pg_default;
 ALTER TABLE users
     OWNER to postgres;
 `
-
 _,err = db.Exec(TableCreate)
 if err != nil {
 	panic(err)
@@ -64,7 +51,6 @@ insert, insertErr := db.Prepare("INSERT INTO users VALUES($1,$2,$3)")
 if insertErr != nil{
 	panic(insertErr)
 }
-
 for _, u := range users{
 	_, err = insert.Exec(u.id,u.name,u.email)
 	if err != nil{
@@ -74,19 +60,16 @@ for _, u := range users{
 	}
 }
 insert.Close()
-
 update, updateErr := db.Prepare("UPDATE users SET Email=$1 WHERE ID=$2")
 if updateErr != nil{
 	panic(updateErr)
 }
-
 _, err = update.Exec("user@packt.com",1)
 if err != nil{
 	panic(err)
 } else{
-	fmt.Println("The user's email address was succesfully updated!")
+	fmt.Println("The user's email address was successfully updated!")
 }
-
 update.Close()
 
 remove, removeErr := db.Prepare("DELETE FROM users WHERE ID=$1")
@@ -97,10 +80,8 @@ _,err = remove.Exec(2)
 if err != nil{
 	panic(err)
 }else{
-	fmt.Println("The second user was succeesfully removed!")
+	fmt.Println("The second user was successfully removed!")
 }
-
 remove.Close()
 db.Close()
 }
-
