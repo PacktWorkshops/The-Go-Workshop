712891c5c1205e1ee47b045c9721c7ad87625648
Andrew Hayes <andrew.hayes@puppetlabs.com>
2019-11-11 18:38:26 +0000

    Updates Chapter 18 with spelling and formatting tweaks

--- Chapter18/Exercise18.02/main.go
+++ Chapter18/Exercise18.02/main.go
@@ -3,5 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Pact")
+	fmt.Println("Hello Packt")
 }
--- Chapter18/Exercise18.03/main.go
+++ Chapter18/Exercise18.03/main.go
@@ -4,5 +4,10 @@ package main
 
 func
 main(){
+  firstVar := 1
+       secondVar :=    2
+ 
+  fmt.Println(firstVar)
+                  fmt.Println(secondVar)
 	fmt.    Println("Hello Packt")
-}
+                    }
