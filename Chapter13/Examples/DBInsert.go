bd661397858301cbb923f42beb5e58f8b0882f48
PournamiJ <52200121+PournamiJ@users.noreply.github.com>
2019-11-14 17:04:05 +0530

    Update DBInsert.go

--- Chapter13/Examples/DBInsert.go
+++ Chapter13/Examples/DBInsert.go
@@ -1,24 +1,19 @@
 package main
-
 import "fmt"
 import "database/sql"
 import _ "github.com/lib/pq"
 func main(){
 	db, err := sql.Open("postgres", "user=postgres password=Start!123 host=127.0.0.1 port=5432 dbname=postgres sslmode=disable")
-	
 	if err != nil {
 	  panic(err)
 	}else{
 		fmt.Println("The connection to the DB was successfully initialized!")
 	}
-	
-	
 	insert, err := db.Prepare("INSERT INTO test VALUES ($1, $2)")
 	if err != nil {
 		panic(err)
 	}
-	
-	_,err = insert.Exec(2,"second")
+		_,err = insert.Exec(2,"second")
 	if err != nil {
 		panic(err)
 	} else{
@@ -26,4 +21,3 @@ func main(){
 	}
 	db.Close()
 	}
-		
