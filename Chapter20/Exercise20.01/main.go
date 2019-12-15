6b06c56e3c429f064f31862d1514138a4f8a1f22
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2019-12-15 11:26:09 +1100

    Add unit test for Exercise20.01

--- Chapter20/Exercise20.01/main.go
+++ Chapter20/Exercise20.01/main.go
@@ -1,9 +1,11 @@
 package main
+
 import (
 	"fmt"
 	"math"
 	"reflect"
 )
+
 type circle struct {
 	radius float64
 }
@@ -11,22 +13,23 @@ type rectangle struct {
 	length  float64
 	breadth float64
 }
-func area(input interface{}) {
+
+func area(input interface{}) float64 {
 	inputType := reflect.TypeOf(input)
-	fmt.Println("type is:", inputType.Name())
 	if inputType.Name() == "circle" {
 		val := reflect.ValueOf(input)
 		radius := val.FieldByName("radius")
-		fmt.Println("area is: ", math.Pi*math.Pow(radius.Float(), 2))
+		return math.Pi * math.Pow(radius.Float(), 2)
 	}
 	if inputType.Name() == "rectangle" {
 		val := reflect.ValueOf(input)
 		length := val.FieldByName("length")
 		breadth := val.FieldByName("breadth")
-		fmt.Println("area is: ", length.Float()*breadth.Float())
+		return length.Float() * breadth.Float()
 	}
+	return 0
 }
 func main() {
-	area(circle{radius: 3})
-	area(rectangle{length: 3.1, breadth: 7.2})
+	fmt.Printf("area of circle with radius 3 is : %f\n", area(circle{radius: 3}))
+	fmt.Printf("area of rectangle with length 3 and breadth 7 is : %f\n", area(rectangle{length: 3, breadth: 7}))
 }
