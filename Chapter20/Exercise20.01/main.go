2a7a4f9f2c59325dc0b8363744cff148ad28f6e8
PournamiJ <52200121+PournamiJ@users.noreply.github.com>
2019-12-16 10:55:53 +0530

    Update main.go

--- Chapter20/Exercise20.01/main.go
+++ Chapter20/Exercise20.01/main.go
@@ -1,11 +1,9 @@
 package main
-
 import (
 	"fmt"
 	"math"
 	"reflect"
 )
-
 type circle struct {
 	radius float64
 }
@@ -13,7 +11,6 @@ type rectangle struct {
 	length  float64
 	breadth float64
 }
-
 func area(input interface{}) float64 {
 	inputType := reflect.TypeOf(input)
 	if inputType.Name() == "circle" {
