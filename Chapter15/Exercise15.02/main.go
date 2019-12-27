e8e29787b2c671496810bd0f49ee2448d93533e0
PournamiJ <52200121+PournamiJ@users.noreply.github.com>
2019-12-27 14:36:17 +0530

    Update main.go

--- Chapter15/Exercise15.02/main.go
+++ Chapter15/Exercise15.02/main.go
@@ -1,17 +1,17 @@
 package main
-import (
-"log"
-"net/http"
-)
+  import (
+  "log"
+  "net/http"
+  )
 type hello struct{}
-func(h hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-msg := "<h1>Hello World</h1"
-w.Write([]byte(msg))
+  func(h hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    msg := "<h1>Hello World</h1"
+    w.Write([]byte(msg))
 }
 func main() {
-http.HandleFunc("/chapter1", func(w http.ResponseWriter, r *http.Request) {
-msg := "Chapter 1"
-w.Write([]byte(msg))
+  http.HandleFunc("/chapter1", func(w http.ResponseWriter, r *http.Request) {
+    msg := "Chapter 1"
+    w.Write([]byte(msg))
 })
-log.Fatal(http.ListenAndServe(":8080", hello{}))
+    log.Fatal(http.ListenAndServe(":8080", hello{}))
 }
