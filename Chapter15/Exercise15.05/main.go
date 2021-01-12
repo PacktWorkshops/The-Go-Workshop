a793f84d091fff8fa484735d68cb6e82cc9af981
PacktSupport <76994974+PacktSupport@users.noreply.github.com>
2021-01-13 00:16:36 +0530

    Update main.go

--- Chapter15/Exercise15.05/main.go
+++ Chapter15/Exercise15.05/main.go
@@ -9,7 +9,7 @@ import (
 func main() {
 
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./index.html")
+		http.ServeFile(w, r, ".static-file/index.html")
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
